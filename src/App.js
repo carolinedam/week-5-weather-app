6d01e7b57b3b300919dbae3179f951447c268f3e
carolinedam <61717208+carolinedam@users.noreply.github.com>
2022-07-11 20:40:29 -0600

    Added Github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Caroline D'Ambro and is
+        <a
+          href="https://github.com/carolinedam/week-5-weather-app"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
