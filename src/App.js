a2c7a49508573f7628e507a115e17038d50e7c0c
carolinedam <61717208+carolinedam@users.noreply.github.com>
2022-07-12 12:24:26 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Caroline D'Ambro and is
-        <a
-          href="https://github.com/carolinedam/week-5-weather-app"
-          target="_blank"
-        >
-          {" "}
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Caroline D'Ambro and is
+          <a
+            href="https://github.com/carolinedam/week-5-weather-app"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
